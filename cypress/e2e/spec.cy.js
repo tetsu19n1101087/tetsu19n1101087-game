b142f3f1d57a266d80708b16acdf30321c13dd5e
tetsu19n1101087 <gizensha0331@gmail.com>
2024-02-11 13:49:52 +0000

    e2eテストの追加

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,23 @@
-describe('The Home Page', () => {
-  it('successfully loads', () => {
-    cy.visit('http://localhost:3000') // change URL to match your dev URL
-  })
-})
+describe('E2Eテスト', () => {
+  it('ゲームがクリアでき、その後タイトルに戻ることができる', () => {
+    cy.visit('http://localhost:3000');
+    
+    cy.contains('プレイする').click();
+
+    for (let i = 0; i < 10; i++) {
+      cy.get('[data-testid="character"]').invoke('text').then((key) => {
+        cy.get('body').trigger('keydown', { key });
+      });
+    }
+
+    cy.contains('タイトルに戻る').click();
+  });
+
+  it('プレイ画面からタイトル画面に遷移する', () => {
+    cy.visit('http://localhost:3000');
+    
+    cy.contains('プレイする').click();
+
+    cy.contains('タイトルに戻る').click();
+  });
+});
