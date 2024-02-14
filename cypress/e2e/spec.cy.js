9c05719c3d51335d948773ae86c00bf445e6fd64
tetsu19n1101087 <gizensha0331@gmail.com>
2024-02-14 13:26:05 +0000

    テスト修正

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,8 @@
 describe('E2Eテスト', () => {
   it('ゲームがクリアでき、その後タイトルに戻ることができる', () => {
     cy.visit('http://localhost:3000');
+
+    cy.contains('数字・記号専用のタイピング練習ゲーム').should('exist');
     
     cy.contains('プレイする').click();
 
@@ -10,14 +12,35 @@ describe('E2Eテスト', () => {
       });
     }
 
+    cy.contains('結果').should('exist');
+    cy.get('th').should('have.length', 5);
+    cy.get('td').should('have.length', 5);
+
     cy.contains('タイトルに戻る').click();
+    cy.contains('数字・記号専用のタイピング練習ゲーム').should('exist');
   });
 
   it('プレイ画面からタイトル画面に遷移する', () => {
     cy.visit('http://localhost:3000');
     
     cy.contains('プレイする').click();
+    cy.contains('表示された数字または記号のキーを押してください').should('exist');
 
     cy.contains('タイトルに戻る').click();
+    cy.contains('数字・記号専用のタイピング練習ゲーム').should('exist');
+  });
+
+  it('正解のキーを押すと正解数のカウントが増える', () => {
+    cy.visit('http://localhost:3000');
+
+    cy.contains('プレイする').click();
+
+    cy.contains(/正解数:/).should('have.text', '正解数: 0');
+
+    cy.get('[data-testid="character"]').invoke('text').then((key) => {
+      cy.get('body').trigger('keydown', { key });
+    });
+
+    cy.contains(/正解数:/).should('have.text', '正解数: 1');
   });
 });
