4bf52fcde39e84cc0b4bc48cba02741938366ee6
tetsu19n1101087 <gizensha0331@gmail.com>
2024-12-18 15:25:29 +0900

    StatefulSetドメイン修正

--- api-server/models/result.js
+++ api-server/models/result.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-mongoose.connect('mongodb://tetsu19n1101087-db-service:27017/game').then(() => {
+mongoose.connect('mongodb://tetsu19n1101087-db-0.tetsu19n1101087-db-service.default.svc.cluster.local:27017/test').then(() => {
   console.log('mongoose successfully connects')
 })
 .catch(err => console.log(err));
