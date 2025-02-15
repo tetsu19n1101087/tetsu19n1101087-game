b609744a6130f6ded605b53d4fbfeaa44e7c9e9b
tetsu19n1101087 <gizensha0331@gmail.com>
2025-02-15 15:47:24 +0900

    テスト設定

---
+++ jest.setup.ts
@@ -0,0 +1,14 @@
+import '@testing-library/jest-dom'
+
+// import { server } from './server'
+
+// beforeAll(() => {
+//   // Establish requests interception layer before all tests.
+//   server.listen()
+// })
+
+// afterAll(() => {
+//   // Clean up after all tests are done, preventing this
+//   // interception layer from affecting irrelevant tests.
+//   server.close()
+// });
