6a186ea570600280a91413f81efec70ea0cbccaa
tetsu19n1101087 <gizensha0331@gmail.com>
2025-02-20 23:06:15 +0900

    route handlers のテストを追加

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,18 +1,17 @@
 import '@testing-library/jest-dom';
 import { TextEncoder, TextDecoder } from 'util';
+import { server } from '@/server';
 
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
 
-// import { server } from './server'
+beforeAll(() => {
+  // Establish requests interception layer before all tests.
+  server.listen();
+});
 
-// beforeAll(() => {
-//   // Establish requests interception layer before all tests.
-//   server.listen()
-// })
-
-// afterAll(() => {
-//   // Clean up after all tests are done, preventing this
-//   // interception layer from affecting irrelevant tests.
-//   server.close()
-// });
+afterAll(() => {
+  // Clean up after all tests are done, preventing this
+  // interception layer from affecting irrelevant tests.
+  server.close();
+});
